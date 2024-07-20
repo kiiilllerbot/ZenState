561913436ca398f76c34a9c9bef46aacf8cf4d2e
kiiilllerbot <dev.shimolkhan@gmail.com>
2024-07-20 13:59:50 +0800

    Integrated prisma client & sign up user account.

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,14 +1,28 @@
 import bcrypt from 'bcrypt';
+import prisma from '../lib/prisma.js';
 
 export const register = async (req, res) => {
   // Get data from params
   const { username, email, password } = req.body;
 
-  // Hash the password
-  const hashedPassword = await bcrypt.hash(password, 10)
-  
-  // Create a new user and save to DB
-  console.log(hashedPassword);
+  try {
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10)
+    
+    // Create a new user and save to DB
+    const newUser = await prisma.user.create({
+      data: {
+        username,
+        email,
+        password: hashedPassword
+      }
+    })
+
+    res.status(201).json({ message: "Account created successfully." })
+  } catch(error){
+    console.log("Error in creating Account: ", error);
+    res.status(500).json({ message: "Failed to created Account." })
+  }
 };
 export const login = (req, res) => {};
 export const logout = (req, res) => {};
