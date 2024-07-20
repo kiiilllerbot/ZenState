62e3b8230e1d8000852ea1a25ab4be124084f76d
kiiilllerbot <dev.shimolkhan@gmail.com>
2024-07-20 14:31:36 +0800

    Integrated JWT, cookie parser, & user login.

--- api/app.js
+++ api/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
 import 'dotenv/config';
 import authRoute from './routes/auth.route.js';
 import postRoute from './routes/post.route.js';
@@ -8,9 +9,10 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
+app.use(cookieParser());
 app.use('/api/posts', postRoute);
 app.use('/api/auth', authRoute);
 
 app.listen(PORT, () => {
-  console.log("Server is running in port: ", PORT);
-})
+  console.log("Server is running on port:", PORT);
+});
--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,28 +1,76 @@
-import bcrypt from 'bcrypt';
-import prisma from '../lib/prisma.js';
+import bcrypt from "bcrypt";
+import 'dotenv/config';
+import jwt from "jsonwebtoken";
+import prisma from "../lib/prisma.js";
 
 export const register = async (req, res) => {
-  // Get data from params
   const { username, email, password } = req.body;
 
   try {
     // Hash the password
-    const hashedPassword = await bcrypt.hash(password, 10)
-    
+    const hashedPassword = await bcrypt.hash(password, 10);
+
     // Create a new user and save to DB
     const newUser = await prisma.user.create({
       data: {
         username,
         email,
-        password: hashedPassword
-      }
-    })
+        password: hashedPassword,
+      },
+    });
 
-    res.status(201).json({ message: "Account created successfully." })
-  } catch(error){
-    console.log("Error in creating Account: ", error);
-    res.status(500).json({ message: "Failed to created Account." })
+    res.status(201).json({ message: "User created successfully" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Failed to create user!" });
   }
 };
-export const login = (req, res) => {};
-export const logout = (req, res) => {};
+
+export const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    // Check if user exists
+    const user = await prisma.user.findUnique({
+      where: { username },
+    });
+
+    if (!user) return res.status(400).json({ message: "Invalid Credentials!" });
+
+    // Check if password is correct
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid)
+      return res.status(400).json({ message: "Invalid Credentials!" });
+
+    // Generate cookie token and send to user
+    const age = 1000 * 60 * 60 * 24 * 7;
+
+    const token = jwt.sign(
+      {
+        id: user.id,
+        isAdmin: false,
+      },
+      process.env.JWT_SECRET_KEY,
+      { expiresIn: age }
+    );
+
+    const { password: userPassword, ...userInfo } = user;
+
+    res
+      .cookie("token", token, {
+        httpOnly: true,
+        // secure:true, // For production
+        maxAge: age,
+      })
+      .status(200)
+      .json(userInfo);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Failed to login!" });
+  }
+};
+
+export const logout = (req, res) => {
+  res.clearCookie("token").status(200).json({ message: "Logout Successful" });
+};
